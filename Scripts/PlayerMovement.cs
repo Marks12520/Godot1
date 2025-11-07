a2b16c77489a2eec9db2bb4a7be73b4571d4b7a4
Marcos Mundina Fabregat <discor2008125@gmail.com>
2025-11-07 20:35:51 +0100

    Refine tilesets, player collision, and level design
    Includes:
    - Updated `Platform.tres` and `Ladder.tres` with additional tile configurations and adjustments.
    - Improved collision shapes in `player.tscn`.
    - Added `Area2D` node and collision signals to `player.tscn`.
    - Integrated a new tileset `Decorations.tres` into `level1.tscn` and reorganized background layers.

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public partial class PlayerMovement : CharacterBody2D
 	private float jumpVelocity = -500.0f;
 	private AnimatedSprite2D as2d;
 
+	private bool allowClimb;
+
 	public override void _Ready()
 	{
 		as2d = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
@@ -41,14 +43,37 @@ public partial class PlayerMovement : CharacterBody2D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
 		}
-
+		
 		// Climbing
-		//if ()
+		if (allowClimb && direction != Vector2.Zero)
+		{
+			velocity.Y = direction.Y * speed;
+		}
+		else if (allowClimb)
+		{
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, speed);
+		}
 		
 		Velocity = velocity;
 		MoveAndSlide();
 	}
-
+	
+	private void _on_area_2d_body_entered(TileMapLayer TML)
+	{
+		if (TML.Name == "LadderLayer")
+		{
+			allowClimb = true;
+		}
+	}
+	
+	private void _on_area_2d_body_exited(TileMapLayer TML)
+	{
+		if (TML.Name == "LadderLayer")
+		{
+			allowClimb = false;
+		}
+	}
+	
 	private void FlipCharacter(Vector2 direction)
 	{
 		if (MathF.Sign(direction.X) == 1)
@@ -60,4 +85,5 @@ public partial class PlayerMovement : CharacterBody2D
 			as2d.FlipH = true;
 		}
 	}
+
 }
